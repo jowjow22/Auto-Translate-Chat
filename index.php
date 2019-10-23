16847e825981d1afeaed2a3da2bf4237c50be6d1
jowjow22 <jonathan224santos@gmail.com>
2019-10-22 21:03:03 -0300

    commit inicial do projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- materialize css -->
+	<link rel="stylesheet" type="text/css" href="css/estiloLogin.css">
+	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/compiler/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="node_modules/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="node_modules/material-icons/iconfont/material-icons.css">
+</head>
+<body>
+
+<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/popper.js/dist/umd/popper.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+</body>
+</html>
