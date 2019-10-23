f052e1495876c7285d1dca460dfaeb1e08c90040
jowjow22 <jonathan224santos@gmail.com>
2019-10-23 01:05:42 -0300

    design do chat quase completo

--- index.php
+++ index.php
@@ -3,13 +3,102 @@
 <head>
 	<title></title>
 	<!-- materialize css -->
-	<link rel="stylesheet" type="text/css" href="css/estiloLogin.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/compiler/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="node_modules/font-awesome/css/font-awesome.css">
 	<link rel="stylesheet" href="node_modules/material-icons/iconfont/material-icons.css">
 </head>
 <body>
 
+<div class="container">
+	<div class="row no-gutters">
+		<div class="col-md-4 border-right">
+			 <div class="user-bar">
+				<img src="img/hmgaiss.jpg" class="user-image">
+				<h6>O nome do cara</h6><br>
+				<div class="status online"></div><p class="text-muted">Online</p>
+			</div>
+				<span class="user-bar-icons float-right">
+					<i class="material-icons">cached</i>
+          			<i class="material-icons">message</i>
+          			<i class="material-icons">menu</i>
+				</span>
+				<div class="search">
+					<input type="text" name="">
+				</div>
+				<div class="friend-messages" data-spy="scroll">
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status online"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status online"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status offline"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status online"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status online"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+					<div class="friend">
+					<img src="img/hm.jpg" class="user-image float-left">
+					<div class="status online"></div>
+					<span class="time float-right">2:40</span>
+					<h6>Gumel gay</h6>
+					<p class="text-muted">minha ultima mensagem é esse nude</p>
+					</div>
+					<hr>
+				</div>	
+			</div>
+			<div class="col-md-8">
+				<div class="atual-chat">
+					<img src="img/b.jpg" class="user-image">
+					<h6>Web Namoradita</h6>
+					<div class="status online"></div><p class="text-muted">Online</p>
+					<span class="atual-chat-icons float-right">
+						<i class="material-icons">video_call</i>
+	          			<i class="material-icons">phone</i>
+	          			<i class="material-icons">settings_applications</i>
+					</span>
+				</div>
+				<div class="chat-area">
+					<div class="messages-area"></div>
+						<form class="send-messages-area">
+							<textarea></textarea>
+						</form>
+						<input type="hidden" name="">https://codepen.io/FilipRastovic/pen/pXgqKK</input>
+						
+				</div>
+			</div>
+		</div>
+	</div>
 <script src="node_modules/jquery/dist/jquery.js"></script>
 <script src="node_modules/popper.js/dist/umd/popper.js"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
